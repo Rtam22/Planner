6915f6a863a56b5a03d4c4c9944d392939651e79
Regan <regantam92@gmail.com>
2025-06-16 23:27:01 +1000

    Added function to compared 2 dates

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -22,17 +22,25 @@ export function addSixToDays(date: Date) {
 
 export function getDayAndDayNumber(selectedDate: Date) {
   const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const today = new Date();
   const dates: CalendarDayProps[] = [];
   let dateForDisplay = new Date(selectedDate);
   for (let i = 0; i < 7; i++) {
     dates.push({
       day: dayNames[dateForDisplay.getDay()],
       dayDate: dateForDisplay.getDate(),
+      isToday: isSameDate(today, dateForDisplay),
     });
     dateForDisplay.setDate(dateForDisplay.getDate() + 1);
   }
 
   return dates;
+}
+
+export function isSameDate(dateA: Date, dateB: Date) {
+  const a = convertToDDMMYYYY(dateA);
+  const b = convertToDDMMYYYY(dateB);
+  return a === b;
 }
 
 export function convertToDDMMYYYY(date: Date) {
@@ -50,4 +58,17 @@ export function convertTimeString24To12(time: string) {
   const hour = h % 12 === 0 ? 12 : h % 12;
   const period = h < 12 ? "AM" : "PM";
   return `${hour}:${m.toString().padStart(2, "0")} ${period}`;
+}
+
+export function setMonthOfDate(
+  date: Date,
+  type: "prev" | "next",
+  numberOfMonths: number
+) {
+  let newDate = new Date(date);
+  type === "prev"
+    ? newDate.setMonth(newDate.getMonth() + numberOfMonths)
+    : newDate.setMonth(newDate.getMonth() - numberOfMonths);
+
+  return newDate;
 }
